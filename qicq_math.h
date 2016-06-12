1b1196d78e65122c678f47170890aea40da2c5f8
Nate McNamara <nate@mcnamara.net>
2016-06-12 16:12:39 -0400

    add abs(vec)

--- qicq_math.h
+++ qicq_math.h
@@ -5,22 +5,27 @@
 #include <qicq/qicq.h>
 
 namespace qicq {
+  template <class T>
+  auto abs(const vec<T>& x) {
+    return each(static_cast<T(*)(T)>(std::abs))(x);
+  }
+  
   template <class T, std::enable_if_t<!std::is_integral<T>::value>* = nullptr>
   auto cos(const vec<T>& x) {
-    return detail::Each()(static_cast<T(*)(T)>(std::cos))(x);
+    return each(static_cast<T(*)(T)>(std::cos))(x);
   }
   template <class T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
   auto cos(const vec<T>& x) {
-    return detail::Each()(static_cast<double(*)(double)>(std::cos))(x);
+    return each(static_cast<double(*)(double)>(std::cos))(x);
   }
   
   template <class T, std::enable_if_t<!std::is_integral<T>::value>* = nullptr>
   auto sin(const vec<T>& x) {
-    return detail::Each()(static_cast<T(*)(T)>(std::sin))(x);
+    return each(static_cast<T(*)(T)>(std::sin))(x);
   }
   template <class T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
   auto sin(const vec<T>& x) {
-    return detail::Each()(static_cast<double(*)(double)>(std::sin))(x);
+    return each(static_cast<double(*)(double)>(std::sin))(x);
   }
 } // namespace qicq
 
