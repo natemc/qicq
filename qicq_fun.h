d3282092dd8c3ad6c15a867e9312cb7e0d367d8e
Nate McNamara <nate@mcnamara.net>
2016-06-18 14:28:23 -0400

    fuzzy floats for qicq_fun

--- qicq_fun.h
+++ qicq_fun.h
@@ -1,6 +1,9 @@
 #ifndef QICQ_FUN_H
 #define QICQ_FUN_H
 
+#include <algorithm>
+#include <cmath>
+
 namespace qicq {
   namespace detail {
     struct Div {
@@ -8,40 +11,75 @@ namespace qicq {
       auto operator()(const X& x, const Y& y) const { return x/y; }
     };
 
-    // TODO fuzzy match on float & double for all 6 relops
     struct Eq {
+      // TODO precompute these and put literals here
+      // static constexpr double dt = std::exp2(-43);
+      // static constexpr float  ft = std::exp2(-19);
+      
       template <class X, class Y>
       auto operator()(const X& x, const Y& y) const { return x==y; }
-    };
-    
-    struct Ge {
-      template <class X, class Y>
-      auto operator()(const X& x, const Y& y) const { return x>=y; }
-    };
-    
-    struct Gt {
-      template <class X, class Y>
-      auto operator()(const X& x, const Y& y) const { return x>y; }
-    };
-    
-    struct Le {
-      template <class X, class Y>
-      auto operator()(const X& x, const Y& y) const { return x<=y; }
-    };
-    
-    struct Lt {
-      template <class X, class Y>
-      auto operator()(const X& x, const Y& y) const { return x<y; }
-    };
-    
-    struct Minus {
-      template <class X, class Y>
-      auto operator()(const X& x, const Y& y) const { return x-y; }
+      bool operator()(float x, float y) const {
+        return fpeq(x, y, float(std::exp2(-19)));
+      }
+      bool operator()(double x, double y) const {
+        return fpeq(x, y, std::exp2(-43));
+      }
+
+    private:
+      // fuzzy match float and double
+      // 0n~0n inf~inf -inf~-inf
+      // 0 must ~ 0 exactly
+      // else tolerance match
+      template <class T>
+      bool fpeq(T x, T y, T t) const {
+        return
+	  std::isnan(x)    ? std::isnan(y)     :
+	  std::isnan(y)    ? false             :
+	  (x<0) != (y<0)   ? false             :
+	  !std::isfinite(x)? !std::isfinite(y) :
+	  !std::isfinite(y)? false             :
+	  std::abs(x-y) <= t*std::max(std::abs(x),std::abs(y));
+      }
     };
     
     struct Ne {
       template <class X, class Y>
       auto operator()(const X& x, const Y& y) const { return x!=y; }
+      bool operator()(float x, float y) const { return !(Eq()(x,y)); }
+      bool operator()(double x, double y) const { return !(Eq()(x,y)); }
+    };
+    
+    struct Lt {
+      template <class X, class Y>
+      auto operator()(const X& x, const Y& y) const { return x<y; }
+      bool operator()(float x, float y) const { return x<y && Ne()(x,y); }
+      bool operator()(double x, double y) const { return x<y && Ne()(x,y); }
+    };
+    
+    struct Ge {
+      template <class X, class Y>
+      auto operator()(const X& x, const Y& y) const { return x>=y; }
+      bool operator()(float x, float y) const { return !(Lt()(x,y)); }
+      bool operator()(double x, double y) const { return !(Lt()(x,y)); }
+    };
+    
+    struct Gt {
+      template <class X, class Y>
+      auto operator()(const X& x, const Y& y) const { return x>y; }
+      bool operator()(float x, float y) const { return (Lt()(y,x)); }
+      bool operator()(double x, double y) const { return (Lt()(y,x)); }
+    };
+    
+    struct Le {
+      template <class X, class Y>
+      auto operator()(const X& x, const Y& y) const { return x<=y; }
+      bool operator()(float x, float y) const { return !(Lt()(y,x)); }
+      bool operator()(double x, double y) const { return !(Lt()(y,x)); }
+    };
+    
+    struct Minus {
+      template <class X, class Y>
+      auto operator()(const X& x, const Y& y) const { return x-y; }
     };
     
     struct Plus {
