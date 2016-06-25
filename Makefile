3aed2a29e2c9b4ef980d0734f025b49c3afec53d
Nate McNamara <nate@mcnamara.net>
2016-06-25 15:51:39 -0400

    add qabs, qcos, qsin

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ install: libqicq.dylib qicq.h qicq_adapt.h qicq_fun.h qicq_lambda.h qicq_math.h 
 	mkdir -p /usr/local/include/qicq
 	cp $(filter %.h,$^) /usr/local/include/qicq
 
-libqicq.dylib: qicq.o qicq_fun.o qicq_sym.o
+libqicq.dylib: qicq.o qicq_fun.o qicq_math.o qicq_sym.o
 	clang++ -shared $^ -o $@
 
 qicq.o: qicq.cpp qicq.h
@@ -24,8 +24,11 @@ qicq.o: qicq.cpp qicq.h
 qicq_fun.o: qicq_fun.cpp qicq_fun.h
 	clang++ $(FLAGS) $(INC) -c $(filter %.cpp,$^)
 
+qicq_math.o: qicq_math.cpp qicq.h qicq_math.h
+	clang++ $(FLAGS) $(INC) -c $(filter %.cpp,$^)
+
 qicq_sym.o: qicq_sym.cpp qicq_sym.h 
 	clang++ $(FLAGS) $(INC) -c $(filter %.cpp,$^)
 
-qicq_test: qicq_test.cpp qicq.o qicq_fun.o qicq_adapt.h qicq_lambda.h qicq_math.h qicq_sym.o hunit.h hunit.o
+qicq_test: qicq_test.cpp hunit.o qicq.o qicq_fun.o qicq_math.o qicq_sym.o hunit.h qicq_adapt.h qicq_lambda.h qicq_math.h qicq_sym.h
 	clang++ $(FLAGS) $(INC) -o $@ $(filter %.cpp %.o,$^)
--- qicq_math.h
+++ qicq_math.h
@@ -3,30 +3,56 @@
 
 #include <cmath>
 #include <qicq/qicq.h>
+// #include <type_traits>
 
 namespace qicq {
-  template <class T>
-  auto abs(const vec<T>& x) {
-    return each(static_cast<T(*)(T)>(std::abs))(x);
-  }
+  namespace detail {
+    struct Abs {
+      static const size_t arity = 1;
+      template <class T>
+      auto operator()(const vec<T>& x) const {
+        return each(static_cast<T(*)(T)>(std::abs))(x);
+      }
+    };
+    
+    struct Cos {
+      static const size_t arity = 1;
+      template <class T,
+        std::enable_if_t<!std::is_integral<T>::value>* = nullptr>
+      auto operator()(const vec<T>& x) const {
+        return each(static_cast<T(*)(T)>(std::cos))(x);
+      }
+      template <class T,
+        std::enable_if_t<std::is_integral<T>::value>* = nullptr>
+      auto operator()(const vec<T>& x) const {
+        return each(static_cast<double(*)(double)>(std::cos))(x);
+      }
+    };
+    
+    struct Sin {
+      static const size_t arity = 1;
+      template <class T,
+        std::enable_if_t<!std::is_integral<T>::value>* = nullptr>
+      auto operator()(const vec<T>& x) const {
+        return each(static_cast<T(*)(T)>(std::sin))(x);
+      }
+      template <class T,
+        std::enable_if_t<std::is_integral<T>::value>* = nullptr>
+      auto operator()(const vec<T>& x) const {
+        return each(static_cast<double(*)(double)>(std::sin))(x);
+      }
+    };
+  } // namespace detail
   
-  template <class T, std::enable_if_t<!std::is_integral<T>::value>* = nullptr>
-  auto cos(const vec<T>& x) {
-    return each(static_cast<T(*)(T)>(std::cos))(x);
-  }
-  template <class T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
-  auto cos(const vec<T>& x) {
-    return each(static_cast<double(*)(double)>(std::cos))(x);
-  }
-  
-  template <class T, std::enable_if_t<!std::is_integral<T>::value>* = nullptr>
-  auto sin(const vec<T>& x) {
-    return each(static_cast<T(*)(T)>(std::sin))(x);
-  }
-  template <class T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
-  auto sin(const vec<T>& x) {
-    return each(static_cast<double(*)(double)>(std::sin))(x);
-  }
+  template <class T> auto abs(const vec<T>& x) { return detail::Abs()(x); }
+  template <class T> auto cos(const vec<T>& x) { return detail::Cos()(x); }
+  template <class T> auto sin(const vec<T>& x) { return detail::Sin()(x); }
+
+  // Even if you don't using namespace std, this will be ambiguous :-(
+  // extern detail::Abs abs;
+  extern detail::Abs qabs;
+  extern detail::Cos qcos;
+  extern detail::Sin qsin;
 } // namespace qicq
 
 #endif
